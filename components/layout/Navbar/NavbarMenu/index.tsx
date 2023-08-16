43b8aa9694f0e3ea9172125b32f066c891326a9d
emre1n <emre1n@protonmail.com>
2023-08-16 21:20:51 +0300

    add sort categories array by id

--- components/layout/Navbar/NavbarMenu/index.tsx
+++ components/layout/Navbar/NavbarMenu/index.tsx
@@ -9,9 +9,11 @@ import { cn } from '@/lib/utils';
 import { Category } from '@/lib/types';
 
 interface LinkCategory {
+  id: number;
   href: string;
   label: string;
   active: boolean;
+  isDeleted: boolean;
 }
 
 interface NavbarMenuProps {
@@ -22,25 +24,32 @@ const NavbarMenu: React.FC<NavbarMenuProps> = ({ data }) => {
   const pathname = usePathname();
 
   const categories = data.map((category: Category) => ({
+    id: category.id,
     href: `/category/${category.id}`,
     label: category.name,
     active: pathname === `/category/${category.id}`,
+    isDeleted: category.deleted,
   }));
+
+  const sortedCategories = categories.slice().sort((a, b) => a.id - b.id);
 
   return (
     <nav className="mx-6 flex items-center gap-x-4 leg:gap-x-6">
-      {categories.map((category: LinkCategory) => (
-        <Link
-          key={category.href}
-          href={category.href}
-          className={cn(
-            'text-sm font-medium transition-colors hover:text-primary',
-            category.active ? 'text-primary' : 'text-neutral-500'
-          )}
-        >
-          {category.label}
-        </Link>
-      ))}
+      {sortedCategories.map(
+        (category: LinkCategory) =>
+          !category.isDeleted && (
+            <Link
+              key={category.href}
+              href={category.href}
+              className={cn(
+                'text-sm font-medium transition-colors hover:text-primary',
+                category.active ? 'text-primary' : 'text-neutral-500'
+              )}
+            >
+              {category.label}
+            </Link>
+          )
+      )}
     </nav>
   );
 };
