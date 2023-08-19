b0476e076d4f67268cedbca90f5b7930deb11fed
emre1n <emre1n@protonmail.com>
2023-08-19 15:28:14 +0300

    update title and subtitle font weights

--- components/home/Billboard/index.tsx
+++ components/home/Billboard/index.tsx
@@ -15,11 +15,11 @@ const Billboard: React.FC<BillboardProps> = ({ data }) => {
         }}
       >
         <div className="absolute inset-0 bg-black opacity-50"></div>
-        <div className="flex flex-col justify-center items-center text-center gap-y-4 h-full w-full">
-          <h1 className="z-10 text-white font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
+        <div className="flex flex-col justify-center items-center text-center gap-y-2 h-full w-full">
+          <h1 className="z-10 text-white text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
             {data.title}
           </h1>
-          <p className="z-10 text-white">{data.subtitle}</p>
+          <p className="z-10 text-white font-light">{data.subtitle}</p>
         </div>
       </div>
     </div>
