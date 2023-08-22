425abcd7ad05be2ba3012f5a1228d096eb875106
emre1n <emre1n@protonmail.com>
2023-08-22 17:00:56 +0300

    add Featured section

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,16 +1,21 @@
-import { getBillboard } from '@/actions/get-billboard';
-import { getMenuItems } from '@/actions/get-menuItems';
-import Billboard from '@/components/home/Billboard';
-import Container from '@/components/ui/Container';
 import React from 'react';
 
+import { getBillboard } from '@/actions/get-billboard';
+import { getMenuItems } from '@/actions/get-menuItems';
+
+import Container from '@/components/ui/Container';
+import Billboard from '@/components/home/Billboard';
+import Featured from '@/components/home/Featured';
+
 const HomePage = async () => {
-  const menuItems = await getMenuItems({ isFeatured: true });
+  const featuredMenuItems = await getMenuItems({ isFeatured: true });
   const billboard = await getBillboard(1);
-  console.log('menuItems', menuItems);
   return (
     <Container>
       <Billboard data={billboard} />
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <Featured title="Featured Menu" items={featuredMenuItems} />
+      </div>
     </Container>
   );
 };
---
+++ components/home/Featured/index.tsx
@@ -0,0 +1,25 @@
+import MenuItemCard from '@/components/ui/MenuItemCard';
+import NoResults from '@/components/ui/NoResults';
+import { MenuItem } from '@/lib/types';
+import React from 'react';
+
+interface FeaturedProps {
+  title: string;
+  items: MenuItem[];
+}
+
+const Featured: React.FC<FeaturedProps> = ({ title, items }) => {
+  return (
+    <div className="space-y-4">
+      <h3 className="font-bold text-3xl">{title}</h3>
+      {items.length === 0 && <NoResults />}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {items.map(item => (
+          <MenuItemCard key={item.id} data={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
