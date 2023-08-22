e18fca00ac705b1f0771083e9227522747aa12af
emre1n <emre1n@protonmail.com>
2023-08-22 16:57:33 +0300

    add NoResults component

---
+++ components/ui/NoResults/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NoResults = () => {
+  return (
+    <div className="flex flex-col gap-1 items-center justify-center h-full w-full p-2 text-secondary">
+      <p> ̄\_(ツ)_/ ̄</p>
+      <p>No results found</p>
+    </div>
+  );
+};
+
+export default NoResults;
