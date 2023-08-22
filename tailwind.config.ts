06fa3b1ebb6c20884ae5271f8bb9dd7ab0d7bd1a
emre1n <emre1n@protonmail.com>
2023-08-22 16:56:46 +0300

    add new color to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,10 +10,13 @@ const config: Config = {
     extend: {
       colors: {
         primary: '#25282b',
+        secondary: '#737373',
         orange: '#f26521',
         purple: '#9d2862',
         green: '#10a99d',
+        background: '#fff',
         'secondary-bg': '#fff9ea',
+        'element-bg': '#f3f4f6',
       },
     },
   },
