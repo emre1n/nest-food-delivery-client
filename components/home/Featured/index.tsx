944917f7936b5eb30ca16ab576f9eef1c188d9fb
emre1n <emre1n@protonmail.com>
2023-08-23 22:09:34 +0300

    update Featured title font size

--- components/home/Featured/index.tsx
+++ components/home/Featured/index.tsx
@@ -11,7 +11,7 @@ interface FeaturedProps {
 const Featured: React.FC<FeaturedProps> = ({ title, items }) => {
   return (
     <div className="space-y-4">
-      <h3 className="font-bold text-3xl text-primary">{title}</h3>
+      <h3 className="font-bold text-2xl text-primary">{title}</h3>
       {items.length === 0 && <NoResults />}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {items.map(item => (
