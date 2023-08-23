ae74f4939cf449b6508bf46fe962af7bbf6446ab
emre1n <emre1n@protonmail.com>
2023-08-23 22:07:20 +0300

    update padding CategoryMenu

--- components/home/Billboard/index.tsx
+++ components/home/Billboard/index.tsx
@@ -7,7 +7,7 @@ interface BillboardProps {
 
 const Billboard: React.FC<BillboardProps> = ({ data }) => {
   return (
-    <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
+    <div className="p-4 sm:px-6 lg:px-8 rounded-xl overflow-hidden">
       <div
         className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover bg-center bg-gradient-to-b"
         style={{
