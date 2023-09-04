5e2d5325e5df4dd7eefa9e836955d06ceb028311
emre1n <emre1n@protonmail.com>
2023-09-04 14:32:04 +0300

    add suggested menu items to menu item details

--- app/(routes)/item/[menuItemId]/page.tsx
+++ app/(routes)/item/[menuItemId]/page.tsx
@@ -1,6 +1,9 @@
 import { getMenuItem } from '@/actions/get-menuItem';
 import { getMenuItems } from '@/actions/get-menuItems';
+import Container from '@/components/ui/Container';
 import React from 'react';
+import { MenuItem } from '../../../../lib/types';
+import Featured from '@/components/home/Featured';
 
 interface MenuItemPageProps {
   params: {
@@ -17,7 +20,23 @@ async function MenuItemPage({
     categoryId: menuItem?.categoryId,
   });
 
-  return <div>Individual Product</div>;
+  return (
+    <div className="bg-white">
+      <Container>
+        <div className="px-4 py-10 sm:px-6 lg:px-8">
+          <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+            <div>Gallery</div>
+            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">Info</div>
+          </div>
+        </div>
+        <hr className="my-10" />
+        <Featured
+          title="You may also like"
+          items={suggestedItems.slice(0, 4)}
+        />
+      </Container>
+    </div>
+  );
 }
 
 export default MenuItemPage;
